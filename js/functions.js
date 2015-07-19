fab626ba8b1bc0d399add369da68b3dee627963a
Piotr Górak <piotrgorak93@gmail.com>
2015-07-19 12:51:07 +0200

    Less refactoring

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
-    //fade_dishcontainer();
-    //add_margin_description();
+    console.log('ready');
 });
 $(window).load(function () {
     add_margin_description();
+    fade_dishcontainer();
+    load_slider();
+
 });
 
 function fade_dishcontainer() {
@@ -24,5 +26,43 @@ function add_margin_description() {
         console.log($index + " " + $(this).text());
         if ($(this).height() < 38)
             $(this).css("margin-bottom", "20px");
+    });
+}
+function load_slider(){
+    console.log("loading");
+    $('.responsive').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 }
