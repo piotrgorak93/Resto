e00e78433faa93a68281db0b09c966b744f9c820
piotrgorak93 <piotrgorak93@gmailc.om>
2015-12-20 15:49:40 +0100

    rwd

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    gallery_dish_hover();
+    hamburgerMenu();
+    registerMenu();
 });
 
 function add_margin_description() {
@@ -7,6 +10,69 @@ function add_margin_description() {
             $(this).css({
                 "margin-bottom": '20px'
             });
+        }
+    });
+}
+
+function gallery_dish_hover() {
+    $(".gallery-container").children().each(function (index, val) {
+        $(val).on({
+            mouseenter: function () {
+                $(val).find("div").slideDown();
+                $(val).find("div").addClass("gallery-dish-force-flex");
+            },
+            mouseleave: function () {
+                $(val).find("div").slideUp();
+                $(val).find("div").removeClass("gallery-dish-force-flex");
+            }
+        });
+    });
+}
+function hamburgerMenu() {
+    var mobileMenuSelector = $(".mobile-menu");
+    $(".fa-bars").click(function () {
+        if (mobileMenuSelector.is(":visible")) {
+            mobileMenuSelector.slideUp();
+        } else {
+            mobileMenuSelector.slideDown();
+        }
+    })
+}
+function registerMenu() {
+    $("nav").find("li").each(function (index, val) {
+        switch (index) {
+            case 0:
+            case 4:
+                $(val).click(function () {
+                    $('html, body').animate({
+                        scrollTop: $(".menu").offset().top
+                    }, 1000);
+                });
+                break;
+            case 1:
+            case 5:
+                $(val).click(function () {
+                    $('html, body').animate({
+                        scrollTop: $(".featured").offset().top
+                    }, 1000);
+                });
+                break;
+            case 2:
+            case 6:
+                $(val).click(function () {
+                    $('html, body').animate({
+                        scrollTop: $(".gallery").offset().top
+                    }, 1000);
+                });
+                break;
+            case 3:
+            case 7:
+                $(val).click(function () {
+                    $('html, body').animate({
+                        scrollTop: $("footer").offset().top
+                    }, 1000);
+                });
+                break;
         }
     });
 }
